506f72018e4507de6469252b6f9ba10e7e1decbe
1100576 <1100576@11005767ui-MacBook-Pro.local>
2018-02-21 22:53:13 +0900

    Add location of yeonsan, yongho

--- js/grayscale.js
+++ js/grayscale.js
@@ -2,42 +2,37 @@
 //Map
 var gooseo_position = new naver.maps.LatLng(35.244949, 129.0891441);
 var kimhae_position = new naver.maps.LatLng(35.2347115, 128.8566677);
+var yeonsan_position = new naver.maps.LatLng(35.1880584,129.086209);
+var yongho_position = new naver.maps.LatLng(35.1152444,129.1111854);
 
 var map = new naver.maps.Map('map', {
     center: gooseo_position,
     zoom: 11
 });
 
-var gooseo_markerOptions = {
-    position: gooseo_position.destinationPoint(0, 10),
-    map: map,
-    icon: {
-        url: '/img/map-marker.svg',
-        size: new naver.maps.Size(48, 48),
-        origin: new naver.maps.Point(0, 0),
-        anchor: new naver.maps.Point(24, 24)
-    }
-};
 
-var kimhae_markerOptions = {
-    position: kimhae_position.destinationPoint(0, 10),
-    map: map,
-    icon: {
-        url: '/img/map-marker.svg',
-        size: new naver.maps.Size(48, 48),
-        origin: new naver.maps.Point(0, 0),
-        anchor: new naver.maps.Point(24, 24)
-    }
-};
-
+//marker
 var gooseo_gmarker = new naver.maps.Marker({
     position: gooseo_position,
     map: map
 });
+
+
 var kimhae_gmarker = new naver.maps.Marker({
     position: kimhae_position,
     map: map
 });
+
+var yeonsan_gmarker = new naver.maps.Marker({
+    position: yeonsan_position,
+    map: map
+});
+
+var yongho_gmarker = new naver.maps.Marker({
+    position: yongho_position,
+    map: map
+});
+
 
 // image
 var slideIndex = 0;
@@ -111,13 +106,33 @@ showSlides();
   $('#contact-1').click(function() {
     $('#contact-info-1').removeClass("contact-hide");
     $('#contact-info-2').addClass('contact-hide');
+    $('#contact-info-3').addClass("contact-hide");
+    $('#contact-info-4').addClass('contact-hide');
     map.setCenter(gooseo_position);
   });
 
   $('#contact-2').click(function() {
     $('#contact-info-1').addClass("contact-hide");
     $('#contact-info-2').removeClass('contact-hide');
+    $('#contact-info-3').addClass("contact-hide");
+    $('#contact-info-4').addClass('contact-hide');
     map.setCenter(kimhae_position);
+  });
+
+  $('#contact-3').click(function() {
+    $('#contact-info-1').addClass("contact-hide");
+    $('#contact-info-2').addClass('contact-hide');
+    $('#contact-info-3').removeClass("contact-hide");
+    $('#contact-info-4').addClass('contact-hide');
+    map.setCenter(yeonsan_position);
+  });
+
+  $('#contact-4').click(function() {
+    $('#contact-info-1').addClass("contact-hide");
+    $('#contact-info-2').addClass('contact-hide');
+    $('#contact-info-3').addClass("contact-hide");
+    $('#contact-info-4').removeClass('contact-hide');
+    map.setCenter(yongho_position);
   });
 
 })(jQuery); // End of use strict
